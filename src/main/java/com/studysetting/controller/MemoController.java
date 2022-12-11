4f74bfbcd8c7b3ccdeaa59d0d4aa0794e42ffb07
JinSung_Kim <rlawlstjd2404@gmail.com>
2022-12-11 15:28:22 +0900

    간단한 thymeleaf 리스트 화면 구현 및 데이터 바인딩

--- src/main/java/com/studysetting/controller/MemoController.java
+++ src/main/java/com/studysetting/controller/MemoController.java
@@ -3,13 +3,16 @@ package com.studysetting.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.studysetting.domain.memo.dto.MemoEntity;
 import com.studysetting.domain.memo.dto.MemoRepository;
 
-@RestController
+@Controller
+// @RestController
 public class MemoController {
 
 	@Autowired
@@ -17,7 +20,8 @@ public class MemoController {
 	
 	
 	@GetMapping("/")
-	public List<MemoEntity> getAllMemo() {
-		return repo.findAll();
+	public String getAllMemo(Model model) {
+		model.addAttribute("memoList", repo.findAll());
+		return "home";
 	}
 }
