f95f6f9f21baebcdebbc544926dcd5d5907afadc
JinSung_Kim <rlawlstjd2404@gmail.com>
2022-12-11 16:01:42 +0900

    메모 등록 페이지 작업

--- src/main/java/com/studysetting/controller/MemoController.java
+++ src/main/java/com/studysetting/controller/MemoController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.studysetting.domain.memo.dto.MemoEntity;
@@ -20,8 +22,20 @@ public class MemoController {
 	
 	
 	@GetMapping("/")
-	public String getAllMemo(Model model) {
+	public String getHomePage(Model model) {
 		model.addAttribute("memoList", repo.findAll());
 		return "home";
 	}
+
+	@GetMapping("/addMemo")
+	public String getAddMemoPage() {
+		return "addMemo";
+	}
+
+	// @PostMapping("/memo") // 타임리프 쪽에서 이 컨트롤러 찌를꺼임
+	// public String postMemo(@ModelAttribute("newMemo")) {
+		
+
+	// 	return "mockup";
+	// }
 }
