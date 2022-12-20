5e8c9572b57344f0bfc710a706e15a0145d56e39
Kim.JinSung_SAMSUMG_Laptop <rlawlstjd2404@gmail.com>
2022-12-20 10:58:53 +0900

    회원가입 시 안내문구 화면단으로 전달하는 로직 추가

--- src/main/java/com/studysetting/common/TokenConstant.java
+++
@@ -1,5 +0,0 @@
-package com.studysetting.common;
-
-public interface TokenConstant {
-  String TOKENID = "진성짱짱토큰짱짱";
-}
--- src/main/java/com/studysetting/controller/UserController.java
+++ src/main/java/com/studysetting/controller/UserController.java
@@ -11,12 +11,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.studysetting.domain.user.UserEntity;
 import com.studysetting.domain.user.UserRepository;
 import com.studysetting.domain.user.dto.User_req_dto;
 
-@SessionAttributes(names = { "isLogined", "userId", "userEmail" }) // model에 addAttribute할 때 세션에도 저장하라고 명시
+@SessionAttributes(names = { "isLogined", "userId", "userEmail" }) // 이 이름들로 model에 addAttribute할 때 세션에도 저장하라고 명시
 @Controller
 public class UserController {
 
@@ -26,7 +27,6 @@ public class UserController {
   @PostMapping("/login")
   public void login(@ModelAttribute("loginParam") User_req_dto loginParam, HttpServletRequest request,
       HttpServletResponse response) throws Exception {
-
     try {
       UserEntity userData = userRepo.findByUserEmail(loginParam.getUserEmail()).get();
       if (loginParam.getUserPassword().equals(userData.getUserPassword())) {
@@ -43,10 +43,12 @@ public class UserController {
   }
 
   @PostMapping("/signUp")
-  public String signUp(@ModelAttribute("loginParam") User_req_dto loginParam) {
+  public String signUp(@ModelAttribute("loginParam") User_req_dto loginParam, RedirectAttributes redirect) {
     if (userRepo.findByUserEmail(loginParam.getUserEmail()).isPresent()) {
       // 이미 회원가입 된 경우의 핸들링
+      redirect.addFlashAttribute("error", "이미 가입된 이메일입니다.");
     } else {
+      redirect.addFlashAttribute("message", "회원가입이 완료되었습니다.");
       userRepo.save(loginParam.toEntity());
     }
     return "redirect:/";
@@ -55,12 +57,12 @@ public class UserController {
   @GetMapping("/logout")
   public String logout(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
     Cookie[] cookies = httpServletRequest.getCookies();
-    if(cookies!=null) {
-    for (int i = 0; i < cookies.length; i++) {
+    if (cookies != null) {
+      for (int i = 0; i < cookies.length; i++) {
         cookies[i].setMaxAge(0);
         httpServletResponse.addCookie(cookies[i]);
+      }
     }
-  }
     return "redirect:/";
   }
 }
